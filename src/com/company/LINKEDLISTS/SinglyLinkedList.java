c3e41634e864f5177b4308f7cb05532229429e6e
goihemere <goihemere@gmail.com>
2023-06-30 23:39:15 +0100

    singly linked list

---
+++ src/com/company/LINKEDLISTS/SinglyLinkedList.java
@@ -0,0 +1,15 @@
+package com.company.LINKEDLISTS;
+
+public class SinglyLinkedList {
+	
+    Node head; // head of list
+  
+    /* Linked list Node*/
+    class Node {
+        int data;
+        Node next;
+  
+        Node(int d) { data = d; next = null; }
+    }
+
+}
